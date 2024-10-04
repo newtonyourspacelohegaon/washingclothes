dc6c8e57a3d5465073d4d946594906977fd693f5
newtonyourspacelohegaon <lohegaonyourspace@gmail.com>
2024-10-04 15:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for,jsonify
 from pymongo import MongoClient,errors
+from datetime import datetime, timedelta
+
 # Replace the connection string with your MongoDB Atlas credentials
 uri = "mongodb+srv://newuserforlife:newuserforlife@cluster0.fginm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -22,6 +24,10 @@ def index():
 @app.route('/book.html')
 def booking():
     return render_template('book.html')
+
+@app.route('/newUser.html')
+def sign_up():
+    return render_template('newUser.html')
 
 @app.route('/get_available_slots')
 def get_available_slots():
@@ -72,8 +78,78 @@ def handle_form():
         print("Insertion failed: 'user_id' must be unique")
     return f"Form submitted successfully!<br>First Name: {first_name}<br>Last Name: {dateSelect}<br>Floor: {timeSelect}<br>Document inserted with ID: {result}"
 
+@app.route('/action_page.php', methods=['POST'])
+def handle_form():
+    first_name = request.form.get('name')
+    dateSelect = request.form.get('date')
+    timeSelect = request.form.get('time')
+
+    if collection.find_one({"dateSelect": dateSelect, "timeSelect": timeSelect}):
+        return "Error: Time slot already booked."
+    
+    original_date = datetime.strptime(dateSelect, "%Y-%m-%d")
+
+    new_date = original_date + timedelta(days=4)
+
+    data = {
+    'dateSelect': dateSelect,
+    'timeSelect': timeSelect,
+    'resetDate':new_date.strftime("%Y-%m-%d")
+    }
+
+    try:
+        if collection.find_one({"user_id": first_name,"resetDate":{"$exists": True}}):
+            if datetime.today()>=datetime.strptime(collection.find({"user_id": first_name})[0]["resetDate"],"%Y-%m-%d"):
+                result = collection.update_one(
+                    {'user_id': first_name},
+                    {'$set': data}
+                )
+                print(f"Document inserted with ID: {result}")
+            else:
+                tempDateSelect=collection.find({"user_id": first_name})[0]["resetDate"]
+                tempDateBooked = collection.find({"user_id": first_name})[0]["dateSelect"]
+                temptimeSelect = collection.find({"user_id": first_name})[0]["timeSelect"]
+                return f"Error: you can book after {tempDateSelect},you have booked the slot for{temptimeSelect} on {tempDateBooked}"
+        else:
+            result = collection.update_one(
+            {'user_id': first_name},
+            {'$set': data}
+        )
+            print(f"Document inserted with ID: {result}")
+    except errors.DuplicateKeyError:
+        print("Insertion failed: 'user_id' must be unique")
+    return f"Form submitted successfully!<br>First Name: {first_name}<br>Last Name: {dateSelect}<br>Floor: {timeSelect}<br>{result}"
+
+@app.route('/sign_up.php', methods=['POST'])
+def handle_sign():
+    first_name = request.form.get('name')
+    hostelFloor = request.form.get('floor')
+    hostelRoom = request.form.get('room')
+
+    if collection.find_one({"user_id": first_name}):
+        return "Error: The User Already Exist."
+    
+    data = {
+    'user_id': first_name,
+    'floor': hostelFloor,
+    'room': hostelRoom
+    }
+
+    # new_field = {
+    # 'new_field_name': 'new_field_value'  # replace with your field name and value
+    # }
 
 
+    try:
+        # result = collection.update_one(
+        #     {'user_id': first_name},
+        #     {'$set': new_field}
+        # )
+        result = collection.insert_one(data)
+        print(f"Document inserted with ID: {result.inserted_id}")
+    except errors.DuplicateKeyError:
+        print("Insertion failed: 'user_id' must be unique")
+    return f"Form submitted successfully!<br>User ID: {first_name}<br>Floor: {hostelFloor}<br>Room: {hostelRoom}<br>inserted with Id:-{result.inserted_id}"
 
 
 
